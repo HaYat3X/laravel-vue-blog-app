94bd17e09d44ee31efed2f8439815008e59d8322
Hayate_T <hayatesato1@gmail.com>
2023-12-24 09:15:23 +0900

    テーブル名とモデル名を変更

--- server/database/migrations/2023_12_23_234647_create_articles_table.php
+++ server/database/migrations/2023_12_23_234647_create_articles_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('blogs', function (Blueprint $table) {
+        Schema::create('articles', function (Blueprint $table) {
             $table->id();
             $table->foreignId('admin_id')->constrained('admins')->onDelete('cascade');
             $table->string('title')->nullable(false);
@@ -30,6 +30,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('blogs');
+        Schema::dropIfExists('articles');
     }
 };
--- server/database/migrations/2023_12_24_000744_create_tags_table.php
+++ server/database/migrations/2023_12_24_000744_create_tags_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('tags', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('blog_id')->constrained('blogs')->onDelete('cascade');
+            $table->foreignId('article_id')->constrained('articles')->onDelete('cascade');
             $table->string('tag')->nullable(false);
         });
     }
