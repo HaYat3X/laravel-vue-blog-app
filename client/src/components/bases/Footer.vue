75943d2973df87a597f68f01d42cfc8ff7e67561
Hayate_T <hayatesato1@gmail.com>
2023-12-31 22:11:13 +0900

    #39 ユーザ側のヘッダー、フッター、サイドバーの作成

---
+++ client/src/components/bases/Footer.vue
@@ -0,0 +1,94 @@
+<template>
+  <footer>
+    <ul>
+      <li>
+        <a href="#">
+          私について
+        </a>
+      </li>
+
+      <li>
+        <a href="#">
+          ブログについて
+        </a>
+      </li>
+
+      <li>
+        <a href="#">
+          ブログ
+        </a>
+      </li>
+
+      <li>
+        <a href="#">
+          お問い合わせ
+        </a>
+      </li>
+
+      <li>
+        <a href="#">
+          プライバシーポリシー
+        </a>
+      </li>
+    </ul>
+
+    <hr />
+
+    <p>
+      &copy; 2023 Hayate Takeda
+    </p>
+  </footer>
+</template>
+
+<style scoped lang="scss">
+footer {
+  margin-top: 300px;
+  background-color: #212529;
+  padding: 30px 0;
+
+  ul {
+    padding: 0 20px;
+    list-style: none;
+    text-align: center;
+
+    li {
+      a {
+        color: #ffffff;
+        text-decoration: none;
+      }
+    }
+  }
+
+  p {
+    text-align: center;
+    color: #ffffff;
+    font-size: 14px;
+  }
+
+  hr {
+    border-top: 0.5px solid #cad4dc;
+    width: 90%;
+    margin: 20px auto;
+  }
+}
+
+@media only screen and (min-width: 768px) and (max-width: 1023px) {
+  ul {
+    display: flex;
+    list-style: none;
+    gap: 20px;
+    justify-content: center;
+
+  }
+}
+
+@media only screen and (min-width: 1024px) {
+  ul {
+    display: flex;
+    list-style: none;
+    gap: 20px;
+    justify-content: center;
+
+  }
+}
+</style>
--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -4,6 +4,11 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
+      path: '/',
+      name: 'Home',
+      component: () => import('../views/Test.vue')
+    },
+    {
       path: '/session/create',
       name: 'session/create',
       component: () => import('../views/auth/sessions/CreateView.vue')
