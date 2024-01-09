8d2c20043f54651869073050d4031839e1f2eae1
Hayate_T <hayatesato1@gmail.com>
2024-01-09 21:44:57 +0900

    #50 記事詳細データを返すapiの作成

--- server/app/Models/Article.php
+++ server/app/Models/Article.php
@@ -4,8 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Article extends Model
 {
     use HasFactory;
+
+    public function tags(): HasMany
+    {
+        return $this->hasMany(Tag::class);
+    }
 }
