85317da7abaf2f8735e82cacc384c21240c04d76
Hayate_T <hayatesato1@gmail.com>
2024-01-16 13:20:12 +0900

    #67 お問い合わせを保存するAPIの作成

---
+++ server/app/Http/Controllers/contact/postsController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers\contact;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Contact;
+
+class postsController extends Controller
+{
+    /**
+     * お問い合わせされたデータを保存する
+     * @access public
+     * @param Illuminate\Http\Request $request
+     * @return Json
+     * @throws Exception データベースクエリの実行中にエラーが発生した場合
+     */
+    public function submitContact(Request $request)
+    {
+        try {
+            $contact = new Contact();
+            $contact->name = $request->input('name');
+            $contact->email = $request->input('email');
+            $contact->category = $request->input('category');
+            $contact->content = $request->input('content');
+
+            if ($contact->save()) {
+                return response()->json([
+                    'message' => 'お問い合わせが完了しました。'
+                ], 200);
+            } else {
+                return response()->json([
+                    'message' => 'お問い合わせに失敗しました。'
+                ], 409);
+            }
+        } catch (Exception) {
+            return response()->json([
+                'message' => 'サーバー内でエラーが発生しました。'
+            ], 500);
+        }
+    }
+}
