078134ed4c31d0308205a70486de04ffae346736
Hayate12345 <hayatesato3@gmail.com>
2024-03-02 13:36:39 +0900

    所有権の確認

--- client/src/main.ts
+++ client/src/main.ts
@@ -3,8 +3,10 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
+import VueMeta from 'vue-meta'
 
 const app = createApp(App)
 app.use(createPinia())
 app.use(router)
+// app.use(VueMeta)
 app.mount('#app')
